9ff78d2bfc9a35bacbb512eb723727933b34c374
cynthia-r <ppscyn@gmail.com>
2016-05-14 19:11:01 -0700

    V1

---
+++ hangman.js
@@ -0,0 +1,108 @@
+// Declare the list of words
+var words = ["Skywalker", "Tatooine", "Wookiee", "Vader", "Stormtrooper", "Empire", "Rebellion", "Threepio", "Artoo", "Blaster"];
+for(i=0; i<words.length; i++)
+  {
+    words[i] = words[i].toUpperCase();
+  }
+
+// Choose a random word to guess
+var index = Math.floor(Math.random() * words.length);
+var word = words[index];
+
+// Get all elements
+var wordBox = document.getElementById('word');
+var pastGuessesBox = document.getElementById('pastGuesses');
+var missesBox = document.getElementById('misses');
+var resultBox = document.getElementById('result');
+
+// Initialize the game
+var guessedIndices = [];
+var guessedLetters = []; // used?
+var misses = 0;
+
+// Renders the word
+function renderWord(){
+  var currentWord = "";
+  for(i=0; i<word.length; i++){
+    // Render guessed letters
+    if(guessedIndices.indexOf(i) > -1){
+      currentWord += word[i];
+    }
+    // Render masked letters
+    else{
+      currentWord += '-';
+    }
+  }
+  //alert(currentWord);  
+  return currentWord;
+}
+
+// Starts the game
+function start(){
+  // Initialize the word
+  wordBox.innerText = renderWord();
+  
+  // Initialize the counter for misses
+  missesBox.innerText = misses;
+}
+
+// Checks the next guess
+function guess(){
+  // Get the guess
+  var guessInput = document.getElementById('nextGuess');
+  var nextGuess = guessInput.value.toUpperCase();
+  
+  // Skip if letter was already guessed
+  if (guessedLetters.indexOf(nextGuess) > -1){
+    alert("Already guessed!");
+    return;
+  }
+  
+  // Add to the letters already guessed
+  guessedLetters.push(nextGuess);
+    
+  // Loop to find matches in the word
+  var isMatch = false;
+  for (i=0; i< word.length; i++){
+    if (word[i] == nextGuess){
+      guessedIndices.push(i);
+      isMatch = true;
+    }      
+  }
+  
+  /*// Other implementation  
+  var currentIndex = -1;
+  var found = false;
+  do{
+    found = word.indexOf(nextGuess, currentIndex);
+    if (found > -1){
+      guessedIndices.push(found);
+      currentIndex = found + 1;
+    }
+  }while(found > -1);
+  isMatch = currentIndex > 0;*/
+  
+  // Increment counter if guess is a miss
+  if (!isMatch)
+    misses++;
+  
+  // Reset input
+  guessInput.value = "";
+  
+  // Update the current number of misses
+  missesBox.innerText = misses;
+  
+  // Update the word
+  wordBox.innerText = renderWord();
+  
+  // Check if won
+  if (guessedIndices.length == word.length){
+    resultBox.innerText = "You won!";
+    return;
+  }
+        
+  // Check if lost
+  if (misses == 9){
+    resultBox.innerText = "You lost!";
+  }
+}
